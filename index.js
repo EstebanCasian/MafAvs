e4c53019ee92416d510a2858528e013a10b53e0b
Estebanquito <89950381+EstebanCasian@users.noreply.github.com>
2021-10-13 20:39:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+        document.write("Me gustas un chingo");
+        alert("Por ti le pongo ganas a la escuela, te quiero siempre en mi vida.");
+        //console.log("Hola Mundo")
+
+ //var javascript = true;
+ //var edad = 18;
+ //var precio = 9.99;
+ //var precio = '9.99';//
+ //var nombre = 'Pedro';//
+
+// if (precio === '9.99') {
+
+//        console.log(precio);
+//}else{
+//    console.log('No es igual');
+//}
+
+ //console.log(precio);//
